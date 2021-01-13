674c27daf81a42b3f624f96e76688aec219bcd8f
cutty12 <cuttyswiss@protonmail.com>
2021-01-13 20:41:33 +0100

    Update requirment files for env and install

--- app/scripts/send_alert_email.py
+++ app/scripts/send_alert_email.py
@@ -8,4 +8,4 @@ from django.contrib.auth.models import User
 def run():
     for immunization in Immunization.objects.all():
         if immunization.vaccine_expired:
-            send_mail('Alert from Health Passport', 'Hello, One or more vaccines has expired, please login to your Health Passport profile to view the details.', settings.EMAIL_HOST_USER, [immunization.owner.email], fail_silently=False)
+            send_mail('Alert from Health Passport', 'Hello, One or more vaccines has expired, please login to your Health Passport profile to view the details.', settings.EMAIL_HOST_USER, [immunization.owner.email], fail_silently=True)
